97560050a76c6b634da97964b1fe7dffbbc63924
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-06-10 13:20:50 -0500

    LM2

--- Linear Regression.R
+++ Linear Regression.R
@@ -44,7 +44,31 @@ lm.fit = lm(medv~lstat + age, data = Boston)
 summary(lm.fit)
 lm.fit = lm(medv~.,data=Boston)
 summary(lm.fit)
+lm.fit1 = lm(medv~.-age,data=Boston)
+summary(lm.fit1)
 
+# Interaction Terms
+# lstat*age = lstat + age + lstat:age
+summary(lm(medv~lstat*age,data=Boston))
+
+# Non-linear tranformation of the predictors
+# I()
+lm.fit2 = lm(medv~lstat + I(lstat^2),data=Boston)
+summary(lm.fit2)
+
+
+lm.fit = lm(medv~lstat,data=Boston)
+anova(lm.fit,lm.fit2) # The anova() function performs a hypothesis test comparing the two models
+
+par(mfrow=c(2,2))
+plot(lm.fit2)
+
+# use poly() to create polynomial within lm()
+lm.fit5 = lm(medv~poly(lstat,5),data=Boston)
+summary(lm.fit5)
+
+# use log
+summary(lm(medv~log(rm),data=Boston))
 
 
 
