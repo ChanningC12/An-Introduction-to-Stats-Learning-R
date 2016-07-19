71c0bc9239f7387df012156bbf4d89770f6c28e1
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-07-18 23:21:52 -0500

    Unsupervised Learning

---
+++ Unsupervised Learning.R
@@ -0,0 +1,3 @@
+# Supervised Learning: The goal is to predict Y using X1,X2,...,Xp
+# Unsupervised Learning: The goal is to discover interesting things about the measurements on X1,X2,...,Xp
+# Principal Components Analysis and Clustering
